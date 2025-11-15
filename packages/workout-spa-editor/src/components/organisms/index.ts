61cab0938a9ca9831f9618ca91fc74166807f5ad
Pablo Albaladejo <pablo.albaladejo@aircall.io>
2025-11-15 23:43:57 +0100

    wip: export StepEditor

--- packages/workout-spa-editor/src/components/organisms/index.ts
+++ packages/workout-spa-editor/src/components/organisms/index.ts
@@ -1,2 +1,4 @@
+export { StepEditor } from "./StepEditor/StepEditor";
+export type { StepEditorProps } from "./StepEditor/StepEditor";
 export { WorkoutList } from "./WorkoutList/WorkoutList";
 export type { WorkoutListProps } from "./WorkoutList/WorkoutList";
