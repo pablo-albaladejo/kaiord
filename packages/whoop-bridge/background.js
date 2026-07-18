36db59d7a3fe4b0e8563d69ab1f73b822c742dbf
Pablo Albaladejo <pablo.albaladejo.mestre@gmail.com>
2026-07-18 14:27:38 +0200

    chore(whoop-bridge): sync BRIDGE_MANIFEST version to 10.0.0

--- packages/whoop-bridge/background.js
+++ packages/whoop-bridge/background.js
@@ -19,7 +19,7 @@ const WHOOP_DASHBOARD = "https://app.whoop.com/";
 const BRIDGE_MANIFEST = {
   id: "whoop-bridge",
   name: "WHOOP",
-  version: "0.1.0",
+  version: "10.0.0",
   protocolVersion: PROTOCOL_VERSION,
   capabilities: ["read:body", "read:sleep"],
 };
